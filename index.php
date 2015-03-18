b8e53c802e35abcff6db938f80b0ab40b608a1ae
Mike <mikederksen5@live.nl>
2015-03-18 11:08:15 +0100

    Index geupdate en css toegevoegd

--- index.php
+++ index.php
@@ -11,7 +11,21 @@
 
     foreach($results as $row)
     {
-        echo $row['Product_ID'] . "<br/>" . $row['Titel'] . "<br/>" . $row['Omschrijving'] . "<br/><br/>";
+        echo "<div class='productOverzichtPositioner'>
+                <div class='productOverzichtContainer'>
+                    <div class='productOverzichtTitle'>
+                        <p>" . $row['Titel'] . "</p>
+                    </div>
+                    <div class='productOverzichtPrijs'>
+                        <p>&euro;" . $row['Prijs'] . "</p>
+                    </div>
+                    <div class='productOverzichtBeschrijving'>
+                        <div style='clear:both'></div>
+                        <p>Talogus te maken. Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting. Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software zoals Aldus PageMaker die versies van Lorem Ipsum bevatten.</p>
+                        <div style='clear:both'></div>
+                    </div>
+                </div>
+            </div>";
     }
 
     ?>

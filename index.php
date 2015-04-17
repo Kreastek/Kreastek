0765de0920416dd922bc1a31eebe982d5aa943d2
Mike <mikederksen5@live.nl>
2015-04-17 09:19:37 +0200

    Shit veranderd enz.. jWZ YOLO

--- index.php
+++ index.php
@@ -6,24 +6,33 @@
 <?php startblock('body') ?>
     <?php
     $results = Database::getProducten();
-    foreach($results as $row)
-    { ?>
-        <div class='productOverzichtPositioner'>
-            <div class='productOverzichtContainer'>
-            <div class='productOverzichtImg' style='background-image: url(<?php echo $row['Afbeelding'] ?>)'></div>
-                <div class='productOverzichtTitle'>
-                    <p><?php echo $row['Titel'] ?></p>
-                </div>
-                <div class='productOverzichtPrijs'>
-                    <p>&euro; <?php echo $row['Prijs'] ?></p>
-                </div>
-                <div class='productOverzichtBeschrijving'>
-                    <div style='clear:both'></div>
-                    <p><?php echo $row['Omschrijving'] ?></p>
+
+    if($results != null)
+    {
+        foreach ($results as $row)
+        { ?>
+            <div class='productOverzichtPositioner'>
+                <div class='productOverzichtContainer'>
+                    <div class='productOverzichtImg'
+                         style='background-image: url(<?php echo $row['Afbeelding'] ?>)'></div>
+                    <div class='productOverzichtTitle'>
+                        <p><?php echo $row['Titel'] ?></p>
+                    </div>
+                    <div class='productOverzichtPrijs'>
+                        <p>&euro; <?php echo $row['Prijs'] ?></p>
+                    </div>
+                    <div class='productOverzichtBeschrijving'>
+                        <p><?php echo $row['Omschrijving'] ?></p>
+                    </div>
                 </div>
                 <div style='clear:both'></div>
             </div>
-        </div>
+        <?php
+        }
+    }
+    else
+    { ?>
+        <p>Het verzoek naar de database heeft geen resultaten opgeleverd, of de verbinding met de database is offline.</p>
     <?php } ?>
 <?php endblock() ?>
 
